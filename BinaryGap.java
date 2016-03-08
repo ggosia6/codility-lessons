6689761bb54f41da2270a5a5c4a291d2bf7b309e
ggosia6 <slayer01@o2.pl>
2016-03-08 19:49:42 +0100

    Create BinaryGap.java

---
+++ BinaryGap.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int N) {
+        int max_gap = 0;
+        int tmp_gap = 0;
+        int tmp = 0;
+        while(N > 0){
+            tmp = N % 2;
+            if(tmp == 1 && tmp_gap > 0){
+                if(tmp_gap > max_gap){
+                    max_gap= tmp_gap;
+                }
+                tmp_gap = 0;
+            }
+            if(tmp == 0){
+                tmp_gap++;
+            }
+            N >>= 1;        
+        }
+        return max_gap;
+    }
+}
