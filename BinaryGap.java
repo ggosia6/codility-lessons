26fac91226a5ff87a242b3ac43aee3905245a097
ggosia6 <slayer01@o2.pl>
2016-03-08 19:57:57 +0100

    Update BinaryGap.java
    Improved from 80% to 100% corectness

--- BinaryGap.java
+++ BinaryGap.java
@@ -3,15 +3,19 @@ class Solution {
         int max_gap = 0;
         int tmp_gap = 0;
         int tmp = 0;
+        boolean one = false;
         while(N > 0){
             tmp = N % 2;
+            if(tmp == 1){
+                one = true;
+            }
             if(tmp == 1 && tmp_gap > 0){
                 if(tmp_gap > max_gap){
                     max_gap= tmp_gap;
                 }
                 tmp_gap = 0;
             }
-            if(tmp == 0){
+            if(tmp == 0 && one){
                 tmp_gap++;
             }
             N >>= 1;        
