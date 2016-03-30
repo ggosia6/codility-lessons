1589dde3eff946c319f50a68b03a8c69ad9f86cb
ggosia6 <slayer01@o2.pl>
2016-03-30 21:48:20 +0200

    Update FrogJmp.java

--- FrogJmp.java
+++ FrogJmp.java
@@ -1,1 +1,12 @@
-/* Count minimal number of jumps from position X to Y */
+/*
+ * Task: Frog Jmp
+ * Source: codility.com - Lesson 03 - https://codility.com/programmers/task/frog_jmp/
+ * Count minimal number of jumps from position X to Y.
+ */
+
+class Solution {
+    /* Return minimal number of frog jumps by D from position X to Y */
+    public int solution(int X, int Y, int D) {
+        return (int)Math.ceil((double)(Y-X)/D);
+    }
+}
