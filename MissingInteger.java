970d5b053a129f2f4aeff3044d5e1c30ea6c2d64
ggosia6 <ggosia6@gmail.com>
2016-04-26 17:13:18 +0200

    Update MissingInteger.java
    Improved from 0% to 55%

--- MissingInteger.java
+++ MissingInteger.java
@@ -1,31 +1,37 @@
-/* Task: 
-   Source: codility.com - Lesson 04
- * Desc
- */
-
 import java.util.HashMap;
-import java.util.Iterator;
 
 class Solution {
-    /* This method returns the minimal positive integer */
     public int solution(int[] A) {
-        int value = 0;
-        int number = 0;
         HashMap<Integer, Integer> cache = new HashMap<Integer, Integer>();
-        for(int i=1;i<=A.length;i++){
-            cache.put(i,0);    
-        }
-        for(int i=0;i<A.length;i++){
-            value = cache.get(A[i]) + 1;
-            cache.put(A[i],value);
-        }
-        Iterator<Integer> keySetIterator = cache.keySet().iterator(); 
-        while(keySetIterator.hasNext()){ 
-            Integer key = keySetIterator.next();
-            if(cache.get(key) == 0) {
-                number = key; break;
-                }
+        int value = 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int N = A.length;
+        for(int i=0;i<N;i++){
+            if(cache.containsKey(A[i])){
+                value = cache.get(A[i]) + 1;
+            } else {
+                value = 1;
             }
-        return number;
+            cache.put(A[i],value);
+            if(A[i] < min){ min = A[i];}
+            if(A[i] > max){ max = A[i];}
+        }
+        // Single element
+        if(N == 1){
+			if(A[0] == 1)
+				return 2;
+			else
+				return 1;
+		}
+        if(max <= 0 || min > 1){
+            return 1;    
+        }
+        for(int j=min;j<=max;j++){
+            if(!cache.containsKey(j)){
+                return j;    
+            }    
+        }
+        return 0;
     }
 }
