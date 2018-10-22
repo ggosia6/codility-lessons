d0949a515587c02318f8274b182cf08b9297195b
Gosia <ggosia6@gmail.com>
2018-10-22 14:50:59 +0200

    Update MaxProductOfThree.java

--- MaxProductOfThree.java
+++ MaxProductOfThree.java
@@ -1,3 +1,9 @@
+/*
+ * Task: MaxProductOfThree
+ * Source: codility.com - Lesson 06 - https://app.codility.com/programmers/lessons/6-sorting/max_product_of_three/
+ * Maximize A[P] * A[Q] * A[R] for any triplet (P, Q, R).
+ */
+
 import java.util.*;
 
 class Solution {
